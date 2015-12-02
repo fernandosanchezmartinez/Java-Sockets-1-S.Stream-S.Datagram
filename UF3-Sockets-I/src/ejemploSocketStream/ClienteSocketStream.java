8d692b996c8e1c7cf28689978c3cccbff27e9cc6
PedroCamacho <pedro.camacho@live.u-tad.com>
2015-12-02 18:17:55 +0100

    Root-Commit

---
+++ UF3-Sockets-I/src/ejemploSocketStream/ClienteSocketStream.java
@@ -0,0 +1,28 @@
+package ejemploSocketStream;
+
+import java.io.*;
+import java.net.*;
+
+public class ClienteSocketStream {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Creando socket cliente");
+			Socket clientSocket = new Socket();
+			System.out.println("Estableciendo la conexión");
+			InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
+			clientSocket.connect(addr);
+			InputStream is = clientSocket.getInputStream();
+			OutputStream os = clientSocket.getOutputStream();
+			System.out.println("Enviando mensaje");
+			String mensaje = "mensaje desde el cliente";
+			os.write(mensaje.getBytes());
+			System.out.println("Mensaje enviado");
+			System.out.println("Cerrando el socket cliente");
+			clientSocket.close();
+			System.out.println("Terminado");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
